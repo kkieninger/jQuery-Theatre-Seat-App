36176d6a75947c16300aec44dfa15e2c57eb5934
Pam Luangrath <pam.luangrath@gmail.com>
2015-09-09 16:55:58 -0400

    Initial seats

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+var seatWrapper = document.getElementById('seat-wrapper');
+var form = document.getElementById('form');
+var theatre = {
+	name: 'Petite Cirque Theatre',
+	numberOfSeats: 24
+}
+function createSeats(){
+	for (var i = 1; i < theatre.numberOfSeats+1; i++){
+		var element = document.createElement('div');
+		element.id = "seat_"+i;
+		element.className= 'available';
+		element.innerHTML = i;
+		seatWrapper.appendChild(element);
+	}
+};
+createSeats();
