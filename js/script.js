643ac41031b67ee3765ee1770c92905c46f5e893
Pam Luangrath <pam.luangrath@gmail.com>
2015-09-10 00:05:21 -0400

    jQuery Seats

--- js/script.js
+++ js/script.js
@@ -1,17 +1,13 @@
-
-var seatWrapper = document.getElementById('seat-wrapper');
-var form = document.getElementById('form');
-var theatre = {
-	name: 'Petite Cirque Theatre',
-	numberOfSeats: 24
-}
-function createSeats(){
-	for (var i = 1; i < theatre.numberOfSeats+1; i++){
-		var element = document.createElement('div');
-		element.id = "seat_"+i;
-		element.className= 'available';
-		element.innerHTML = i;
-		seatWrapper.appendChild(element);
+$(document).ready(function(){
+	var theatre = {
+		numberOfSeats: 24
 	}
-};
-createSeats();
+	function createSeats(){
+		for (var i = 1; i < theatre.numberOfSeats+1; i++){
+			var seatId = "seat" + i;
+			var $newSeat = $('<div class="available" id="' + seatId + '">'+ i +'</div>');
+			$('#seat-wrapper').append($newSeat);
+		}
+	};
+	createSeats();
+});
