11d61b28dc379712adb08ce70a5e128cf3314c13
Benjamin <sgtpwnd@gmail.com>
2015-09-10 10:03:58 -0400

    commiting minor changes

--- js/script.js
+++ js/script.js
@@ -7,21 +7,9 @@ var theatre = {
 	numberOfSeats: 24
 }
 
-function createSeats(){
-	for (var i = 1; i < theatre.numberOfSeats+1; i++){
-		var element = document.createElement('div');
-		element.id = "seat_"+i;
-		element.className= 'available';
-		element.innerHTML = i;
-		seatWrapper.appendChild(element);
-	}
-};
-createSeats();
-$(function(){
-	$("#seat_").on("click", function(){
-		$( this ).fadeOut( "slow");
-	});
-	});
+
+
+
 
 $(document).ready(function(){
 	var theatre = {
@@ -33,12 +21,18 @@ $(document).ready(function(){
 			var $newSeat = $('<div class="available" id="' + seatId + '">'+ i +'</div>');
 			$('#seat-wrapper').append($newSeat);
 			$('#seat-wrapper').on('click', displayForm);
+			$("#seat_" + i).on("click", function(){
+			$( this ).fadeOut( "slow");
+	});
+
 		}
+
 	};
 	createSeats();
 	function displayForm(){
 		$('#form').slideDown('fast');
 		$('#formButton').on('click', submitForm);
+
 	}
 	function submitForm(){
 		$('#form').slideUp('fast');
