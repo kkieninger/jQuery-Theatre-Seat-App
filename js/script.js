55554ae5d040df44e82b0594ba6f5f1ad9d8bf16
Pam Luangrath <pam.luangrath@gmail.com>
2015-09-10 09:59:16 -0400

    Merging

--- js/script.js
+++ js/script.js
@@ -4,18 +4,23 @@ $(document).ready(function(){
 	}
 	function createSeats(){
 		for (var i = 1; i < theatre.numberOfSeats+1; i++){
-			var seatId = "seat" + i;
+			var seatId = "seat_" + i;
 			var $newSeat = $('<div class="available" id="' + seatId + '">'+ i +'</div>');
 			$('#seat-wrapper').append($newSeat);
 			$('#seat-wrapper').on('click', displayForm);
 		}
 	};
 	createSeats();
-	function displayForm(){
+	function displayForm(seatId){
 		$('#form').slideDown('fast');
+		$('#chosenSeat').html('You have chosen to reserve seat ' + this.seatId + '.');
 		$('#formButton').on('click', submitForm);
 	}
 	function submitForm(){
 		$('#form').slideUp('fast');
 	}
+
+
+
+
 });
