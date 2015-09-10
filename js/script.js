7772e5f3169f7895e3e2107d0bbc748c14c4c617
Pam Luangrath <pam.luangrath@gmail.com>
2015-09-10 00:14:31 -0400

    Form slide down/up jQuery

--- js/script.js
+++ js/script.js
@@ -7,7 +7,15 @@ $(document).ready(function(){
 			var seatId = "seat" + i;
 			var $newSeat = $('<div class="available" id="' + seatId + '">'+ i +'</div>');
 			$('#seat-wrapper').append($newSeat);
+			$('#seat-wrapper').on('click', displayForm);
 		}
 	};
 	createSeats();
+	function displayForm(){
+		$('#form').slideDown('fast');
+		$('#formButton').on('click', submitForm);
+	}
+	function submitForm(){
+		$('#form').slideUp('fast');
+	}
 });
